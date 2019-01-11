6b5339c892d67364ba914d460b042a6edb76b748
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2019-01-11 11:51:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -547,17 +547,9 @@ if (message.content.startsWith(prefix + 'help')) { /// This is The DMS Code Send
 『!cv <name> / انشاء رووم فويس』
 『!delet <name> / مسح الشات او الرووم فويس』
 『❖!ccolors <number> / ينشا لك الوان مع كم الوان تبي』
-   `,`
-        ***__Music orders__***
-**
-『{prefix}play / لتشغيل أغنية برآبط أو بأسم』
-『{prefix}skip / لتجآوز الأغنية الحآلية』
-『{prefix}pause / إيقآف الأغنية مؤقتا』
-『{prefix}resume / لموآصلة الإغنية بعد إيقآفهآ مؤقتا』
-『{prefix}vol / لتغيير درجة الصوت 100 - 0』
-『{prefix}stop / لإخرآج البوت من الروم』
-『{prefix}np / لمعرفة الأغنية المشغلة حآليا』
-『{prefix}queue / لمعرفة قآئمة التشغيل』
+『لمعرفه الكريدت لسه ما انضافت』
+`,`
+ 
 **
         ***__Games orders__***
  **       
